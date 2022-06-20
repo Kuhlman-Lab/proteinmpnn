e7cb0fbc812c27e7691f701918593b9360702384
Nick Randolph (Laptop) <nzrandol@unc.edu>
2022-06-20 08:52:39 -0400

    Ran testing experiment with af2 formatted output flag

--- run/thanh/mutres_redesign/run_design.sh
+++ run/thanh/mutres_redesign/run_design.sh
@@ -4,6 +4,6 @@
 #conda activate mpnn
 
 #python /proj/kuhl_lab/proteinmpnn/run/generate_json.py @json.flags
-python ../../../generate_json.py @json.flags
+python ../../generate_json.py @json.flags
 #python /proj/kuhl_lab/proteinmpnn/run/run_protein_mpnn.py @proteinmpnn_res_specs.json
-python ../../../run_protein_mpnn.py @proteinmpnn.flags
+python ../../run_protein_mpnn.py @proteinmpnn.flags
