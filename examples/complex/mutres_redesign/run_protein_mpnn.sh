fa23023d3cacc944eb07b8298ee83d066230a8c8
Nick Randolph (Laptop) <nzrandol@unc.edu>
2022-06-20 09:45:38 -0400

    Removed thanh's results in favor of examples

---
+++ examples/complex/mutres_redesign/run_protein_mpnn.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#SBATCH -p volta-gpu
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem=16g
+#SBATCH -t 00-00:30:00
+#SBATCH --qos gpu_access
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=rhel8
+#SBATCH --mail-type=END
+#SBATCH --mail-user=user@email.com
+
+#source ~/.bashrc
+#conda activate mpnn
+
+python ../../../run/generate_json.py @json.flags
+
+python ../../../run/run_protein_mpnn.py @proteinmpnn.flags
