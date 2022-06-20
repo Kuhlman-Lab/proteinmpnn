59f82cbb03b9e75120b32897d10865d8f069cc24
Nick Randolph (Laptop) <nzrandol@unc.edu>
2022-06-19 23:13:33 -0400

    Modified function to separate reading and writing.

--- run/helper_scripts/assign_fixed_chains.py
+++ run/helper_scripts/assign_fixed_chains.py
@@ -1,17 +1,23 @@
 import argparse
+import json
 
-def main(args):
-    import json
-
-    with open(args.input_path, 'r') as json_file:
+def get_json_list(input_path):
+    with open(input_path, 'r') as json_file:
         json_list = list(json_file)
+
+    dict_list = []
+    for json_str in json_list:
+        dict_list.append(json.loads(json_str))
+
+    return dict_list
+
+def main(json_list, chain_list):
     
     global_designed_chain_list = []
-    if args.chain_list != '':
-        global_designed_chain_list = [str(item) for item in args.chain_list.split()]
+    if chain_list != '':
+        global_designed_chain_list = [str(item) for item in chain_list.split()]
     my_dict = {}
-    for json_str in json_list:
-        result = json.loads(json_str)
+    for result in json_list:
         all_chain_list = [item[-1:] for item in list(result) if item[:9]=='seq_chain'] #['A','B', 'C',...]
         if len(global_designed_chain_list) > 0:
             designed_chain_list = global_designed_chain_list
@@ -21,7 +27,10 @@ def main(args):
         fixed_chain_list = [letter for letter in all_chain_list if letter not in designed_chain_list] #fix/do not redesign these chains 
         my_dict[result['name']]= (designed_chain_list, fixed_chain_list)
     
-    with open(args.output_path, 'w') as f:
+    return my_dict
+
+def write_json(output_path, my_dict):
+    with open(output_path, 'w') as f:
         f.write(json.dumps(my_dict) + '\n')
 
 
@@ -32,7 +41,9 @@ if __name__ == "__main__":
     argparser.add_argument("--chain_list", type=str, default='', help="List of the chains that need to be designed")    
 
     args = argparser.parse_args()
-    main(args) 
+    json_list = get_json_list(args.input_path)
+    my_dict = main(json_list, args.chain_list)
+    write_json(args.output_path, my_dict)
 
 # Output looks like this:
 # {"5TTA": [["A"], ["B"]], "3LIS": [["A"], ["B"]]}
