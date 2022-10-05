4c6357c6de93ef27603d2554ae1b140a9bc2c3fc
Nick Randolph (longleaf) <nzrandol@unc.edu>
2022-10-04 22:20:54 -0400

    Updated slurm submission script

--- sandbox_outs/novozyme/run_stability_pred.sh
+++ sandbox_outs/novozyme/run_stability_pred.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-source activate mpnn
+#SBATCH -J stability_pred
+#SBATCH -p volta-gpu
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH --mem=16g
+#SBATCH -t 01-00:00:00
+#SBATCH --qos gpu_access
+#SBATCH --gres=gpu:1
+#SBATCH --output=slurm-%j-%x.out
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=nzrandol@unc.edu
 
-python ../stability_pred.py --num_seq_per_target 2 --batch_size 2 --test_set_path test.csv --pdb_path wildtype_structure_prediction_af2.pdb --sort_by_rank
+source ~/.bashrc
+module add cuda/11.2
+conda activate mpnn
+python ../stability_pred.py --num_seq_per_target 5000 --batch_size 25 --test_set_path test.csv --pdb_path wildtype_structure_prediction_af2.pdb --sort_by_rank
